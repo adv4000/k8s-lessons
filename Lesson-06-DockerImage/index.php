375a63895b01804f4b9e8ab47e974d75617b9d7c
Denis Astahov <adv4000@gmail.com>
2020-05-19 11:43:35 -0700

    Lesson to Build and Push DockerImage to DockerHub

---
+++ Lesson-06-DockerImage/index.php
@@ -0,0 +1,7 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello From Kubernetes</h1><br>";
+echo "Server IP Address is: $ip_server", "<br><p>";
+echo "Made by <font color=blue>Denis Astahov";
+?>
